d13c866e7e94b61ee38c09031241bd5d31d278cc
GEORGECHZ <georgemichael_chavez@ucol.mx>
2022-05-09 18:52:10 -0500

    chatSockets_v1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+const app = express()
+
+const http = require ("http")
+const server = http.createServer(app)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/cliente/index.html`);
+})
+
+server.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+})
