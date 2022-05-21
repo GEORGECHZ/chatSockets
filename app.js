327e8db76dd4e8e6a0e2e7be3a9f996c3cd2b292
GEORGECHZ <georgemichael_chavez@ucol.mx>
2022-05-20 20:32:22 -0500

    V2

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const { Socket } = require("dgram")
 const express = require ("express")
 const app = express()
 
 const http = require ("http")
 const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    // console.log('Un usuario se ha conectado')
+    // socket.on('disconnect', () => {
+    //     console.log('Un usuario se ha desconectado')
+    // })
+    // socket.on('chat', (msg) => {
+    //     console.log('Mensaje: '+ msg)
+    // })
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg);
+    })
+})
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/cliente/index.html`);
